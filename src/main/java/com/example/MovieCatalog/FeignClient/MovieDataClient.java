e7cb3f58656aa9d5f23bd63c191aa4b5f25cecbe
Shrikant Jagdale <ShrikantJagdale1111@gmail.com>
2020-03-16 09:02:55 -0700

    Feign client

--- src/main/java/com/example/MovieCatalog/FeignClient/MovieDataClient.java
+++ src/main/java/com/example/MovieCatalog/FeignClient/MovieDataClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("http://103.132.244.132:8083")
+@FeignClient(name = "movie-data" , value = "movie-data", url = "http://127.0.0.1:8083")
 //@RibbonClient("localhost:8083")
 public interface MovieDataClient {
 	
